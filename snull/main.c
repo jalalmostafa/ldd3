484b25b1cdb9ad66e1a01bb3c830659f509a81be
Jalal Mostafa <jalal.a.mostapha@gmail.com>
2023-03-23 16:22:00 +0100

    snull: debugging napi poll 20

--- snull/main.c
+++ snull/main.c
@@ -99,7 +99,7 @@ struct snull_packet_tx* snull_get_tx_buffer(struct net_device* dev)
 
     spin_lock_irqsave(&priv->lock, flags);
     pkt = priv->txq.ppool;
-    pr_debug("BEFORE ppool: %x - head: %x - pkt: %x - pkt->next: %x\n", priv->txq.ppool ?: 1, priv->txq.head, pkt, pkt->next);
+    pr_debug("BEFORE ppool: %p - head: %p - pkt: %p - pkt->next: %p\n", priv->txq.ppool ?: 1, priv->txq.head, pkt, pkt->next);
     if (!pkt) {
         pr_debug("Out of Pool\n");
         goto out;
@@ -115,7 +115,7 @@ struct snull_packet_tx* snull_get_tx_buffer(struct net_device* dev)
     pkt->next = priv->txq.head;
     priv->txq.head = pkt;
 
-    pr_debug("AFTER ppool: %x - head: %x - pkt: %x - pkt->next: %x\n", priv->txq.ppool, priv->txq.head, pkt, pkt->next);
+    pr_debug("AFTER ppool: %p - head: %p - pkt: %p - pkt->next: %p\n", priv->txq.ppool, priv->txq.head, pkt, pkt->next);
 out:
     spin_unlock_irqrestore(&priv->lock, flags);
     return pkt;
@@ -127,12 +127,12 @@ void snull_release_tx(struct snull_packet_tx* pkt)
     struct snull_priv* priv = netdev_priv(pkt->dev);
 
     spin_lock_irqsave(&priv->lock, flags);
-    pr_debug("BEFORE ppool: %x - head: %x - pkt: %x - pkt->next: %x\n", priv->txq.ppool, priv->txq.head, pkt, pkt->next);
+    pr_debug("BEFORE ppool: %p - head: %p - pkt: %p - pkt->next: %p\n", priv->txq.ppool, priv->txq.head, pkt, pkt->next);
 
     priv->txq.head = pkt->next;
     pkt->next = priv->txq.ppool;
     priv->txq.ppool = pkt;
-    pr_debug("AFTER ppool: %x - head: %x - pkt: %x - pkt->next: %x\n", priv->txq.ppool, priv->txq.head, pkt, pkt->next);
+    pr_debug("AFTER ppool: %p - head: %p - pkt: %p - pkt->next: %p\n", priv->txq.ppool, priv->txq.head, pkt, pkt->next);
 
     spin_unlock_irqrestore(&priv->lock, flags);
 
