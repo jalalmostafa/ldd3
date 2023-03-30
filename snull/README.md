f10af729dc29e2230b297ec97588d85adc17ac6b
Jalal Mostafa <jalal.a.mostapha@gmail.com>
2023-03-30 19:16:05 +0200

    snull: add xdp tests

--- snull/README.md
+++ snull/README.md
@@ -1,3 +1,7 @@
 # SNULL Network Driver
 
 Progress: LDD3 - Chapter 17
+
+## XDP test compilation
+
+`apt install clang llvm libelf-dev libpcap-dev gcc-multilib build-essential linux-tools-common linux-tools-generic linux-headers-$(uname -r) libbpf-dev`
---
+++ snull/xdp/xdp-pass.c
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+SEC("xdp")
+int  xdp_prog_simple(struct xdp_md *ctx)
+{
+    bpf_printk("bpf_printk: %x\n", ctx->data);
+	return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
